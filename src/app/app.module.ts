b916e5f276dfb939a93c6031860c68780093f74b
Devin Mounts, Matt Smith and Rita Bennett-Chew <devin+matt+rita@>
2018-08-22 14:24:48 -0700

    add firebase config and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsDetailsComponent } from './projects-details/projects-details.component';
 import { ProjectsNewComponent } from './projects-new/projects-new.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -16,7 +28,12 @@ import { ProjectsNewComponent } from './projects-new/projects-new.component';
     ProjectsNewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/project.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Project } from './models/project.model'
+
+@Injectable()
+export class ProjectService {
+  projects: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.projects = database.list('projects');
+  }
+}
